02e0162f86eab790e8b908fd098e894ab47570e7
Hyunsu Cho <phcho@nvidia.com>
2026-09-10 19:03:37 -0700

    Update cpp/cmake/thirdparty/get_nvforest.cmake

--- cpp/cmake/thirdparty/get_nvforest.cmake
+++ cpp/cmake/thirdparty/get_nvforest.cmake
@@ -42,8 +42,8 @@ endfunction()
 # To use a different nvForest locally, set the CMake variable
 # CPM_nvforest_SOURCE=/path/to/local/nvforest
 find_and_configure_nvforest(VERSION          ${CUML_MIN_VERSION_nvforest}
-        FORK             rapidsai
-        PINNED_TAG       ${rapids-cmake-checkout-tag}
+        FORK             hcho3
+        PINNED_TAG       fcbb3b04ff05fd1e28881b398b9c38dd884537c7
         EXCLUDE_FROM_ALL ${CUML_EXCLUDE_NVFOREST_FROM_ALL}
         # When PINNED_TAG above doesn't match cuml,
         # force local nvforest clone in build directory
